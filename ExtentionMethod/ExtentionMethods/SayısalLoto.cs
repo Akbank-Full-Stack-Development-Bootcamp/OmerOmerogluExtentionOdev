b07e0a5c0cf46e37771af158919be68ba3fc79bc
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-06-06 19:57:32 +0300

    Add project files.

---
+++ ExtentionMethod/ExtentionMethods/SayısalLoto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExtentionMethod.ExtentionMethods
+{
+    public static class SayısalLoto
+    {
+        public static int PlayLoto(this int num)
+        {
+            Random num1 = new Random(); return num1.Next(1, 10);
+
+
+        }
+
+       
+    }
+}
---
+++ ExtentionMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExtentionMethod.ExtentionMethods;
+
+namespace ExtentionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1=0;
+            int LuckyNumber = num1.PlayLoto();
+            Console.WriteLine(LuckyNumber);
+
+        }
+    }
+}
